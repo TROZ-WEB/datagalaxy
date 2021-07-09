964c38aa265804c65eef976a25cc77b124953eb3
romain.simon <romain.simon@thetribe.io>
2021-07-09 13:14:44 +0000

    Merged PR 7564: feat: add linter and prettier configuration
    feat: add linter and prettier configuration

--- packages/chromium-extension/src/popup/Popup.tsx
+++ packages/chromium-extension/src/popup/Popup.tsx
@@ -1,8 +1,6 @@
-import React from "react";
+import React from 'react';
 import styles from './Popup.css';
 
-const Popup = () => {
-  return <div className={styles.root}>{chrome.i18n.getMessage("welcomeMessage")}</div>;
-}
+const Popup = () => <div className={styles.root}>{chrome.i18n.getMessage('welcomeMessage')}</div>;
 
 export default Popup;
