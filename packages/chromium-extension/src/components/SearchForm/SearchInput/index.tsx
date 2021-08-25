98633f43dfc24b6e9bcb125813a864cd5be25412
Simon Galet <simon.galet@thetribe.io>
2021-08-25 18:10:21 +0200

    feat(chromium): Search input style
    Improve the search input style
    
    https://datagalaxy.atlassian.net/browse/EXAPP-26

--- packages/chromium-extension/src/components/SearchForm/SearchInput/index.tsx
+++ packages/chromium-extension/src/components/SearchForm/SearchInput/index.tsx
@@ -2,6 +2,7 @@ import React, { ComponentPropsWithRef, forwardRef, ReactElement } from 'react';
 import CheckMark from '../../../icons/CheckMark';
 import Close from '../../../icons/Close';
 import Refresh from '../../../icons/Refresh';
+import Search from '../../../icons/Search';
 import styles from './index.css';
 
 export interface IProps extends ComponentPropsWithRef<'input'> {
@@ -12,27 +13,33 @@ export interface IProps extends ComponentPropsWithRef<'input'> {
 
 const SearchInput = forwardRef<HTMLInputElement, IProps>(
     ({ onClearSearch, loading = false, success = false, ...props }, ref) => {
-        let rightElement: ReactElement = null;
+        let leftElement: ReactElement = null;
 
         if (loading) {
-            rightElement = (
-                <div className={styles.Right}>
+            leftElement = (
+                <div className={styles.Left}>
                     <Refresh className={styles.IconLoading} />
                 </div>
             );
         } else if (success) {
-            rightElement = (
-                <div className={styles.Right}>
+            leftElement = (
+                <div className={styles.Left}>
                     <CheckMark className={styles.IconSuccess} />
+                </div>
+            );
+        } else {
+            leftElement = (
+                <div className={styles.Left}>
+                    <Search />
                 </div>
             );
         }
 
         return (
             <div className={styles.Root}>
-                <input className={styles.Input} {...props} ref={ref} />
+                {leftElement}
 
-                {rightElement}
+                <input className={styles.Input} {...props} ref={ref} />
 
                 {props.value && (
                     <div className={styles.Right}>
--- packages/chromium-extension/src/icons/Search.tsx
+++ packages/chromium-extension/src/icons/Search.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 
 function Search(props: React.SVGProps<SVGSVGElement>) {
     return (
-        <svg fill="none" height={20} viewBox="0 0 20 20" width={20} xmlns="http://www.w3.org/2000/svg" {...props}>
+        <svg height="1em" viewBox="0 0 20 20" width="1em" xmlns="http://www.w3.org/2000/svg" {...props}>
             <path
                 d="M8.333 1.745h-.01a6.667 6.667 0 104.108 11.918l4.479 4.504 1.178-1.178-4.494-4.495a6.614 6.614 0 001.404-4.09 6.66 6.66 0 00-6.66-6.659h-.007.002zm0 11.667a5 5 0 115-5 5.006 5.006 0 01-5 5z"
-                fill="#fff"
+                fill="currentColor"
+                fillRule="evenodd"
             />
         </svg>
     );
