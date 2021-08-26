0ff20290decca36d9c8d464ffa7ee3dc00a6e902
Romain Simon <romain.simon@thetribe.io>
2021-08-26 10:11:55 +0200

    fix: remove console.info

--- packages/chromium-extension/src/components/Breadcrumb/index.tsx
+++ packages/chromium-extension/src/components/Breadcrumb/index.tsx
@@ -3,8 +3,6 @@ import { formatBreadcrumb } from 'shared';
 import styles from './index.css';
 
 const Breadcrumb = ({ path }: { path: string }) => {
-    console.info(formatBreadcrumb);
-
     const formattedPath = useMemo(() => formatBreadcrumb(path), [path]);
 
     return <div className={styles.Root}>{formattedPath}</div>;
