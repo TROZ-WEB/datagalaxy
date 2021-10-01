5945b006ece96dad7705b22940e82471ad7b375f
Simon <simon.galet@thetribe.io>
2021-10-01 16:21:34 +0000

    Merged PR 8435: feat(chromium): Vimeo Find PAT
    feat(chromium): Vimeo Find PAT
    
    Find PAT gif replaced with Vimeo videos

--- packages/chromium-extension/src/pages/Onboarding/PatFinder/index.tsx
+++ packages/chromium-extension/src/pages/Onboarding/PatFinder/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '../../../components/ui/Button';
 import OnboardingLayout from '../Layout';
-import FindPATEn from '../../../../assets/find-pat-en.gif';
-import FindPATFr from '../../../../assets/find-pat-fr.gif';
 import styles from './index.css';
 
 const PatFinder = () => {
@@ -14,9 +12,25 @@ const PatFinder = () => {
             <div className={styles.Root}>
                 <p className={styles.Header}>{chrome.i18n.getMessage('onboarding_login_helpPAT')}</p>
                 {chrome.runtime.getManifest().current_locale.startsWith('en') ? (
-                    <img alt="How to find PAT" className={styles.FindPatGif} src={FindPATEn} />
+                    <iframe
+                        allow="autoplay; fullscreen; picture-in-picture"
+                        frameBorder="0"
+                        height="360"
+                        src="https://player.vimeo.com/video/618892114?h=5a4697f7d5&color=ffffff&byline=0"
+                        title="Find PAT Video"
+                        width="640"
+                        allowFullScreen
+                    />
                 ) : (
-                    <img alt="How to find PAT" className={styles.FindPatGif} src={FindPATFr} />
+                    <iframe
+                        allow="autoplay; fullscreen; picture-in-picture"
+                        frameBorder="0"
+                        height="360"
+                        src="https://player.vimeo.com/video/618892015?h=5a4697f7d5&color=ffffff&byline=0"
+                        title="Find PAT Video"
+                        width="640"
+                        allowFullScreen
+                    />
                 )}
                 <Button onClick={() => history.goBack()}>
                     {chrome.i18n.getMessage('onboarding_patFinder_closeButton')}
