a00714d0bf62b5bdb8046e199ef2554ae453acd9
Tommy <tommy.alexandre@thetribe.io>
2022-03-11 15:36:56 +0100

    fix token refresh

--- packages/shared/src/Http/index.ts
+++ packages/shared/src/Http/index.ts
@@ -17,23 +17,18 @@ export async function http<T>(request: Request): Promise<HttpResponse<T>> {
 
     response.parsedBody = await response.json();
 
-    if (!response.ok) {
-        // Trigger custom error for unauthorized error (accessToken no more valid)
-        if (response.status === 401) {
-            clonedRequest.headers.set(
-                'authorization',
-                `Bearer ${await AccessToken.getInstance().refreshAccessToken()}`,
-            );
-            const clonedResponse: HttpResponse<T> = await fetch(request);
+    // Trigger custom error for unauthorized error (accessToken no more valid)
+    if (response.status === 401) {
+        clonedRequest.headers.set('authorization', `Bearer ${await AccessToken.getInstance().refreshAccessToken()}`);
+        const clonedResponse: HttpResponse<T> = await fetch(request);
 
-            clonedResponse.parsedBody = await clonedResponse.json();
+        clonedResponse.parsedBody = await clonedResponse.json();
 
-            if (!clonedResponse.ok) {
-                throw new Error(clonedResponse.statusText);
-            }
-        } else {
-            throw new Error(response.statusText);
+        if (!clonedResponse.ok) {
+            throw new Error(clonedResponse.statusText);
         }
+    } else {
+        throw new Error(response.statusText);
     }
 
     return response;
